b0bd4bdebbf7aa5d0bd0151e6a9cf7957266975b
10hokageee <nikita.serdyukov.20033002@gmail.com>
2024-05-02 03:34:23 +0300

    added video player

--- gulpfile.js
+++ gulpfile.js
@@ -6,7 +6,9 @@ const uglify = require('gulp-uglify');
 const imagemin = require('gulp-imagemin');
 const browserSync = require('browser-sync').create();
 const del = require('del');
-
+const svgSprite = require('gulp-svg-sprite');
+const cheerio = require('gulp-cheerio');
+const replace = require('gulp-replace');
 
 function browsersync() {
   browserSync.init({
@@ -48,11 +50,74 @@ return src('app/images/**/*.*')
 function scripts() {
   return src([
     'node_modules/jquery/dist/jquery.js',
-    'app/js/main.js'])
+    'node_modules/@fancyapps/fancybox/dist/jquery.fancybox.js',
+    'app/js/main.js',
+  ])
   .pipe( concat('main.min.js'))
   .pipe(uglify())
   .pipe(dest('app/js'))
   .pipe(browserSync.stream())
+}
+
+function svgSprites() {
+  return src('app/images/icons/*.svg') 
+    .pipe(
+      svgSprite({
+        mode: {
+          stack: {
+            sprite: '../sprite.svg', 
+          },
+        },
+      })
+    )
+		.pipe(dest('app/images')); 
+}
+
+function svgSprites() {
+  return src('app/images/icons/*.svg') 
+  .pipe(cheerio({
+        run: ($) => {
+            $("[fill]").removeAttr("fill"); 
+            $("[stroke]").removeAttr("stroke"); 
+            $("[style]").removeAttr("style"); 
+        },
+        parserOptions: { xmlMode: true },
+      })
+  )
+	.pipe(
+	      svgSprite({
+	        mode: {
+	          stack: {
+	            sprite: '../sprite.svg', 
+	          },
+	        },
+	      })
+	    )
+	.pipe(dest('app/images')); 
+}
+
+function svgSprites() {
+  return src('app/images/icons/*.svg') 
+  .pipe(cheerio({
+        run: ($) => {
+            $("[fill]").removeAttr("fill"); 
+            $("[stroke]").removeAttr("stroke"); 
+            $("[style]").removeAttr("style"); 
+        },
+        parserOptions: { xmlMode: true },
+      })
+  )
+	.pipe(replace('&gt;','>'))  
+	.pipe(
+	      svgSprite({
+	        mode: {
+	          stack: {
+	            sprite: '../sprite.svg', 
+	          },
+	        },
+	      })
+	    )
+	.pipe(dest('app/images')); 
 }
 
 function build() {
@@ -72,6 +137,7 @@ function watching() {
 watch(['app/scss/**/*.scss'], styles);
 watch(['app/js/**/*.js', '!app/js/main.min.js'], scripts);
 watch(['app/**/*.html']).on('change', browserSync.reload);
+watch(['app/images/icons/*.svg'], svgSprites);
 }
 
 
@@ -82,7 +148,8 @@ exports.browsersync = browsersync;
 exports.watching = watching;
 exports.images = images;
 exports.cleanDist = cleanDist;
+exports.svgSprites = svgSprites;
 
 exports.build = series(cleanDist, images, build);
 
-exports.default = parallel(styles, scripts, browsersync, watching);
+exports.default = parallel(svgSprites, styles, scripts, browsersync, watching);
